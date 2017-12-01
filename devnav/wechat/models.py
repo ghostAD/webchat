65b41d91d6809cae08c0bc35021691abf33da693
MemoryAndDream <312141830@qq.com>
2017-12-01 13:29:51 +0800

    up

--- devnav/wechat/models.py
+++ devnav/wechat/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+# Create your models here.
+
+
+class Reply(models.Model):
+    reply = models.CharField(max_length=100)
+    weight = models.IntegerField()
+    update_time = models.DateTimeField('events time')
+    create_time = models.DateTimeField(auto_now=True)  # 创建时间(自动获取当前时间)
+
+    def __str__(self):  # 将对象以str的方式显示出来
+        return self.reply
+
