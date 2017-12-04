5dbd08fe2e376f239b3efda00d03171c98009c8b
MemoryAndDream <312141830@qq.com>
2017-12-04 16:06:46 +0800

    up

--- devnav/wechat/models.py
+++ devnav/wechat/models.py
@@ -20,3 +20,15 @@ class Reply(models.Model):
             # 在Python3中使用 def __str__(self):
             return self.reply
 
+class Resource(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=255)
+    post_date =  models.CharField(max_length=255)
+    request_count=  models.IntegerField(default=1)
+    download_url = models.CharField(max_length=255)
+    update_time = models.DateTimeField('events time')
+    create_time = models.DateTimeField(auto_now=True)  # 创建时间(自动获取当前时间)
+
+    def __unicode__(self): # 将对象以str的方式显示出来
+            # 在Python3中使用 def __str__(self):
+            return self.url+self.title
