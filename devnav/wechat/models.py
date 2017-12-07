7f2076833cb3e8c13194b9ea6085dc5faf28b0b0
MemoryAndDream <312141830@qq.com>
2017-12-07 13:54:34 +0800

    up

--- devnav/wechat/models.py
+++ devnav/wechat/models.py
@@ -21,6 +21,7 @@ class Reply(models.Model):
             return self.reply
 
 class Resource(models.Model):
+    keyword = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     url = models.CharField(max_length=255)
     post_date =  models.CharField(max_length=255,blank=True)
@@ -32,11 +33,34 @@ class Resource(models.Model):
     user = models.CharField(max_length=100,blank=True)#对应api的
     uploader = models.CharField(max_length=100,blank=True)#对应api的
     type = models.CharField(max_length=100, blank=True)#以,分割各种类型
+    OpenID = models.CharField(max_length=100)
+    UnionID = models.CharField(max_length=100)
+
     def __unicode__(self): # 将对象以str的方式显示出来
             # 在Python3中使用 def __str__(self):
             return self.url+self.title
 
-class user(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=100)
     OpenID = models.CharField(max_length=100)
     UnionID = models.CharField(max_length=100)
+
+class Resource_Cache(models.Model):
+    keyword = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=255)
+    post_date =  models.CharField(max_length=255,blank=True)
+    request_count=  models.IntegerField(default=1)
+    download_url = models.CharField(max_length=255,blank=True)
+    update_time = models.DateTimeField('events time',blank=True)
+    create_time = models.DateTimeField(auto_now=True,blank=True)  # 创建时间(自动获取当前时间)
+    verify_time = models.DateTimeField('verify time',blank=True)
+    user = models.CharField(max_length=100,blank=True)#对应api的
+    uploader = models.CharField(max_length=100,blank=True)#对应api的
+    type = models.CharField(max_length=100, blank=True)#以,分割各种类型
+    OpenID = models.CharField(max_length=100)
+    UnionID = models.CharField(max_length=100)
+
+    def __unicode__(self): # 将对象以str的方式显示出来
+            # 在Python3中使用 def __str__(self):
+            return self.url+self.title
--- devnav/wechat/service/reply.py
+++ devnav/wechat/service/reply.py
@@ -6,7 +6,7 @@
 @file: reply.py 
 @time: 2017/12/1 14:37 
 """
-from ..models import Reply,Resource
+from ..models import Reply,Resource,Resource_Cache
 from django.db.models import Sum, Count,Avg
 import logging
 import datetime
@@ -16,8 +16,8 @@ logger = logging.getLogger('default')
 
 def reply(MsgContent):
     queryResult = search_resource(MsgContent)
-    if queryResult:
-        return {'reply': queryResult, 'mode': 0}
+    if queryResult:#这个逻辑后面得改，不兼容搜索，要么就是根据公众号类型不同返回
+       return {'reply': queryResult, 'mode': 0}
     #如果有资源就返回资源，如果没有就骂人 ,切换模式，模式需要在用户session中记录 输入别骂了才能切换回来 或者设置资源的前缀，不合法的都骂
     #reply = maRen()
     reply = crawler(MsgContent)
@@ -45,7 +45,11 @@ def crawler(keyword):
     urlinfos = rsDict['urlinfos']
     rs = []
     for urlinfo in urlinfos:
-        rs.append("%s %s"%(urlinfo.get('title',''),urlinfo.get('url','')))
+        title = urlinfo.get('title','')
+        url = urlinfo.get('url','')
+        rs.append("%s %s"%(title,url))
+        if title and url:
+            save_resource(title,url,keyword)
 
     result =  '\n'.join(rs)[:590]  #限制貌似是600
     #logger.debug(result)
@@ -72,11 +76,25 @@ def weight_choice(list):
 
 def search_resource(queryString):
     try:
-        resources = Resource.objects.filter(title__icontains=queryString)[:10]#后面需要加更多限制
+        now = datetime.datetime.now()
+        start = now-datetime.timedelta(hours=23, minutes=59, seconds=59)#缓存一天的数据
+        resources = Resource_Cache.objects.filter(create_time__gt=start).filter(keyword__iexact=queryString)[:10]#后面需要加更多限制 反正也显示不了10条
         result=[]
         for resource in resources:
-            result.append(resource.url)
+            result.append(resource.title + ' '+resource.url)
         output = '\n'.join(result)
     except Exception,e:
         logger.error(str(e))
     return output
+
+def save_resource(title,url,keyword,uploader='system'):
+    try:
+        r = Resource_Cache()
+        r.title=title
+        r.url = url
+        r.keyword = keyword
+        r.uploader = uploader
+        r.create_time = datetime.datetime.now()
+        r.save()
+    except Exception,e:
+        logger.error(str(e))
