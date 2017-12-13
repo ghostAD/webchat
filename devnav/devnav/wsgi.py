e052851f5d9bb2042453a3aa1854394bf9410b45
MemoryAndDream <312141830@qq.com>
2017-12-13 13:55:46 +0800

    up

--- devnav/devnav/wsgi.py
+++ devnav/devnav/wsgi.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+from os.path import join,dirname,abspath
 import sys
-path = '/home/meng/webchat/devnav'#这样不好移植，但是用相对路径怎么都是错的，猜想应该是服务器里不一样  因为wsgi的作用是把项目加载给apache2
-
 #wsgi是运行在embeded的模式下，即在apache的worker进程内。对wsgi application的修改，需要重启apache才能生效
-if path not in sys.path:
-	sys.path.append(path)
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, PROJECT_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
